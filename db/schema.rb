e7385603deab31a1ca1f3597e02cddea602cb080
Tylor Schafer <tylorschafer@gmail.com>
2019-08-19 20:38:21 -0600

    Add relationship between reviews and item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190820022121) do
+ActiveRecord::Schema.define(version: 20190820023534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,10 @@ ActiveRecord::Schema.define(version: 20190820022121) do
     t.string "title"
     t.string "content"
     t.integer "rating"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_reviews_on_item_id"
   end
 
   add_foreign_key "items", "merchants"
+  add_foreign_key "reviews", "items"
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,4 +6,8 @@ describe Review, type: :model do
     it { should validate_presence_of :content }
     it { should validate_presence_of :rating }
   end
+
+  describe 'relationships' do
+    it {should belong_to :item}
+  end
 end
