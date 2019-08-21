371e2cb16336a2e66c3bcc452834158d352b362e
Tylor Schafer <tylorschafer@gmail.com>
2019-08-21 08:57:58 -0600

    Reformat new review test to use more spacing

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -1,14 +1,14 @@
 describe "Create new review" do
   describe "when i visit the item show page" do
     before :each do
-      @bike_shop = Merchant.create(
+      @bike_shop = Merchant.create!(
         name: "Brian's Bike Shop",
         address: '123 Bike Rd.',
         city: 'Denver',
         state: 'CO',
         zip: 80203
       )
-      @chain = @bike_shop.items.create(
+      @chain = @bike_shop.items.create!(
         name: "Chain", description: "It'll never break!",
         price: 50,
         image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588",
@@ -18,12 +18,15 @@ describe "Create new review" do
     it "i can create a new review by filling out a form" do
 
       title = "review"
-      content = "theis product is great"
+      content = "this product is great"
       rating = 4
 
       visit "items/#{@chain.id}"
+
       click_link "Add New Review"
+
       expect(current_path).to eq("/items/#{@chain.id}/reviews/new")
+
       fill_in :title, with: title
       fill_in :content, with: content
       fill_in :rating, with: rating
@@ -35,7 +38,6 @@ describe "Create new review" do
       expect(new_review.title).to eq(title)
       expect(new_review.content).to eq(content)
       expect(new_review.rating).to eq(rating)
-
     end
   end
 end
