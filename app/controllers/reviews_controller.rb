d617a83632b8d21acbf3b58274b41ab0abc40cde
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-08-22 14:29:23 -0700

    fixes edit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
   end
   def create
     item = Item.find(params[:id])
-    item.reviews.create(review_params)
+    item.reviews.create!(review_params)
     redirect_to "/items/#{item.id}"
   end
   def review_params
