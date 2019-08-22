d8f1edd225ddc1b9c03cef08efe3ed090108b0dd
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-08-21 19:05:35 -0700

    adds more expect of the test

--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe "as a visitor" do
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @tire.reviews.create(title: "Santi's review", description: "this product is trash", reating: 1)
-
+    end
       visit "/items/#{@tire.id}"
 
       expect(page).to have_link("Edit Review")
@@ -26,7 +26,6 @@ RSpec.describe "as a visitor" do
 
       expect(current_path).to eq("/items/#{@tire.id}/review_edit")
       expect(page).to have_link("Santi's review")
+      expect(find_field('title').value).to eq "Santi's review"
+      expect(find_field('description').value).to eq "this product is trash"
       
-    end
-  end
-end
