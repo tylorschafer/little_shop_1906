8095c8380b86f72b1637c64b7d42f19a78d279a8
Tylor Schafer <tylorschafer@gmail.com>
2019-08-24 19:14:18 -0600

    Refactor seeds and model features

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,16 +4,16 @@ class Review < ApplicationRecord
   validates_presence_of :title, :content
   validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Number must be between 1 and 5"}
 
-  def self.top_rated
-    Review.order(:rating).last(3).reverse
+  def self.top_rated(item_id)
+    all.where("item_id = #{item_id}").order(:rating).last(3).reverse
   end
 
-  def self.bottom_rated
-    Review.order(:rating).first(3)
+  def self.bottom_rated(item_id)
+    all.where("item_id = #{item_id}").order(:rating).first(3)
   end
 
-  def self.average_rating
-    Review.average(:rating)
+  def self.average_rating(item_id)
+    all.where("item_id = #{item_id}").average(:rating)
   end
 
 end
